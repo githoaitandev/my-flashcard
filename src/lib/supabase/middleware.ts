f8be22cd054c43d109e09ce99ea28f33b4a65f4e
Nguyen Hoai Tan <hoaitandev@example.com>
2025-07-13 13:06:49 +0700

    fix the bug loop of redirect login while auth

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -5,13 +5,14 @@ import { createServerClient } from "@supabase/ssr";
 const protectedPaths = ["/decks", "/practice", "/study", "/flashcards", "/"];
 
 // Routes that are only accessible when not logged in
-// const authPaths = ["/auth/login", "/auth/signup", "/auth/forgot-password"];
+const authPaths = ["/auth/login", "/auth/signup", "/auth/forgot-password"];
 
 export async function updateSession(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
     request,
   });
-
+  console.log(request);
+  console.log(request.cookies.getAll());
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -41,6 +42,12 @@ export async function updateSession(request: NextRequest) {
   const isProtectedPath = protectedPaths.some((route) =>
     path.startsWith(route)
   );
+
+  if (authPaths.some((route) => path.startsWith(route))) {
+    return supabaseResponse;
+  }
+
+  console.log(user, isProtectedPath, path);
   if (!user && isProtectedPath) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
